5157e2307e27ee6a2a64b9abc5237b0b50b7e8ff
Fileno <52785172+Fileno@users.noreply.github.com>
2021-02-10 19:29:40 -0300

    Add files via upload
    Upload dos arquivos do meu primeiro programa em Java

---
+++ FirstCode/src/Principal.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/**
+* Meu primeiro programa Java
+*  
+* 
+*
+*
+*/
+
+
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Test  \ntest2");
+		
+		Scanner in = new Scanner (System.in);
+		
+		String nome = in.nextLine();
+		
+		System.out.println(nome);
+		in.close();
+	}
+	
+
+}
